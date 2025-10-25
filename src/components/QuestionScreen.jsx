f690e7d1be53c92880d1a42144feb67efca31b0f
jaffar-ux <hawlaadmi@gmail.com>
2025-10-25 20:30:53 +0530

    Update QuestionScreen.jsx

--- src/components/QuestionScreen.jsx
+++ src/components/QuestionScreen.jsx
@@ -95,7 +95,7 @@ export default function QuestionScreen({ question, onYes, isFirst }) {
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ delay: 0.9, duration: 0.8 }}
                 >
-                    {isFirst ? "I promise it's worth it..." : "Abb merkun like karte..."}
+                    {isFirst ? "I promise it's worth it..." : "App merku like kartin?..."}
                 </motion.p>
 
                 {/* Answer buttons */}
